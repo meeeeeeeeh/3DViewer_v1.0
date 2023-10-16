467f1fbf8e726e0c930bf700fe6e192baebb8f09
Pandion Damian <pandiond@oa-o4.msk.21-school.ru>
2023-10-16 17:13:26 +0300

    new struct

--- src/viewer.c
+++ src/viewer.c
@@ -8,6 +8,7 @@ int main() {
     //data input;
 
     v.matrix_vert = NULL;
+    p.polygons = NULL;
     
     int err_flag = 0;
     file = fopen("abc.obj", "r");
@@ -17,22 +18,25 @@ int main() {
     counter_vert(file, &v);
     counter_pol(file, &p);
     memory_vert(&v);
-    
+    memory_pol(&p);
+
+    get_vertex(file, &v);
+    get_pol(file, &p);
 
     printf("%ld\n", v.amount_vert);
     printf("%ld\n", p.amount_pol);
+    for(long unsigned int i =0; i < v.amount_vert; i++) {
+        for(int j = 0; j < 3; j++) {
+            printf("%f ", v.matrix_vert[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
 
-// int open_obj(char *f_name, FILE *file) {
-//     int err_flag = 0;
-//     file = fopen(f_name, "r");
-//     if(!file) err_flag = 1;
-//     return err_flag;
-// }
 
 void counter_vert(FILE *file, vertex *input_d) {
-    //rewind(file);
+    rewind(file);
     char *buffer = NULL;
     size_t len = MAX_LENGHT;
     input_d->amount_vert = 0;
@@ -84,38 +88,6 @@ void get_vertex(FILE *file, vertex *input_d) {
     }
 }
 
-void find_center(vertex *v, double *center_x, double *center_y, double *center_z) {
-    *center_x = v->minmax_x[0] + (v->minmax_x[1] - v->minmax_x[0]) / 2;
-    *center_y = v->minmax_y[0] + (v->minmax_y[1] - v->minmax_y[0]) / 2;
-    *center_z = v->minmax_z[0] + (v->minmax_z[1] - v->minmax_z[0]) / 2;
-}
-
-// нужно занулить значения вначале
-void fill_min_max(vertex *input_d) {
-    for (unsigned long int i = 0; i < input_d->amount_vert; i++) {
-        if (input_d->matrix_vert[i][0] < input_d->minmax_x[0]) {
-            input_d->minmax_x[0] = input_d->matrix_vert[i][0];
-        }
-        else if (input_d->matrix_vert[i][0] < input_d->minmax_x[1]) {
-            input_d->minmax_x[1] = input_d->matrix_vert[i][0];
-        }
-        
-        if (input_d->matrix_vert[i][1] < input_d->minmax_y[0]) {
-            input_d->minmax_y[0] = input_d->matrix_vert[i][1];
-        }
-        else if (input_d->matrix_vert[i][1] < input_d->minmax_y[1]) {
-            input_d->minmax_y[1] = input_d->matrix_vert[i][1];
-        }
-
-        if (input_d->matrix_vert[i][2] < input_d->minmax_z[0]) {
-            input_d->minmax_x[0] = input_d->matrix_vert[i][2];
-        }
-        else if (input_d->matrix_vert[i][2] < input_d->minmax_z[1]) {
-            input_d->minmax_z[1] = input_d->matrix_vert[i][2];
-        }
-    }
-}
-
 void get_pol(FILE *file, polygon *input_d) {
     rewind(file);
     //int i = 0;
@@ -139,7 +111,7 @@ void get_pol(FILE *file, polygon *input_d) {
             }
 
             input_d->polygons[line] = calloc(count_memory, sizeof(double));
-            line++;
+            
 
             for (int i = 1; buffer[i] != '\0'; i++) {
                 if (is_digit(buffer[i]) && buffer[i - 1] == ' ') {
@@ -156,6 +128,7 @@ void get_pol(FILE *file, polygon *input_d) {
                     new_buf[0] = '\0';
                 }
             }
+            line++;
             count_memory = 0;
             amount_in_pol = 0;
         }
